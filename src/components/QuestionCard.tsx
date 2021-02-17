427861bae778c9cfd75c129aaf167b9dc5fb923d
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-02-17 16:53:24 +0000

    fetch trivia data and shuffle answers

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,35 @@
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNo: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNo,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNo} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default QuestionCard;
